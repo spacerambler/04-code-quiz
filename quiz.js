f5e92e3c87142ecfb03f070b5887ca354cce8fb2
Keely Bailey <keelybailey3@gmail.com>
2021-05-30 16:00:19 -0500

    Start CSS file and event listener

--- quiz.js
+++ quiz.js
@@ -3,11 +3,27 @@ var timerEl = document.getElementById("time")
 timerEl = 60;
 
 function scoreTimer(){
+    //interval
     var timerInterval = setInterval(function(){
         timerEl--;
         timeEl = `${timerEl} + "seconds"`
+
+    //add if for end of timer
 
     }, 1000);
 }
 
 scoreTimer();
+
+var startBtn = document.querySelector(".start-button");
+var container = document.querySelector(".container");
+
+var mode = "is-hidden";
+
+startBtn.addEventListener("click", function(){
+    if (mode === "is-hidden") {
+        mode = "is-shown";
+        container.setAttribute();
+        scoreTimer();
+}
+})
